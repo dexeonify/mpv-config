b07d46a682b8b6c8f07f98b46e73bfd4bfdc4a18
dexeonify <bloonyapprentice@gmail.com>
2021-11-28 11:17:08 +0800

    Update autosave.lua to CyberShadow's fork
    - Suppress "saving state" messages in console
    - Saves the state immediately when a new file is loaded
    - Takes advantage of `delete-watch-later-config` to
      facilitate playlist resuming
    
    More info: mpv-player/mpv@3ccf70b5

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ This is mainly just for backup and keeping track of changes.
 
 ## User scripts
 
-- [autosave.lua](https://gist.github.com/Hakkin/5489e511bd6c8068a0fc09304c9c5a82):
+- [autosave.lua](https://gist.github.com/CyberShadow/2f71a97fb85ed42146f6d9f522bc34ef):
   Periodically saves "watch later" data during playback,
   rather than only saving on quit.
 
--- scripts/autosave.lua
+++ scripts/autosave.lua
@@ -14,7 +14,7 @@
 local options = require 'mp.options'
 
 local o = {
-	save_period = 30
+    save_period = 30
 }
 
 options.read_options(o)
@@ -22,17 +22,34 @@ options.read_options(o)
 local mp = require 'mp'
 
 local function save()
-	mp.command("write-watch-later-config")
+    mp.commandv("set", "msg-level", "cplayer=warn")
+    mp.command("write-watch-later-config")
+    mp.commandv("set", "msg-level", "cplayer=status")
 end
 
 local save_period_timer = mp.add_periodic_timer(o.save_period, save)
 
 local function pause(name, paused)
-	if paused then
-		save_period_timer:stop()
-	else
-		save_period_timer:resume()
-	end
+    save()
+    if paused then
+        save_period_timer:stop()
+    else
+        save_period_timer:resume()
+    end
 end
 
 mp.observe_property("pause", "bool", pause)
+mp.register_event("file-loaded", save)
+
+local function end_file(data)
+    if data.reason == 'eof' or data.reason == 'stop' then
+        local playlist = mp.get_property_native('playlist')
+        for i, entry in pairs(playlist) do
+            if entry.id == data.playlist_entry_id then
+                mp.commandv("delete-watch-later-config", entry.filename)
+                return
+            end
+        end
+    end
+end
+mp.register_event("end-file", end_file)
