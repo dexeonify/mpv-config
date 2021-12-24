c2b5a4d2f7488431d75598d476ec874dd9d6a9b0
dexeonify <bloonyapprentice@gmail.com>
2021-12-24 22:46:04 +0800

    Remove superfluous cursor_position check for backspace()
    Originally, the cursor_position checked is ~= 9. If the cursor is at the
    9th position (which is the last), shift_cursor() won't kick in, aka the
    cursor won't move to the left.
    
    I don't like this behaviour, backspace should always shift the cursor to
    the left even at the last position. My workaround is to increase the
    check to ~= 10, which checks if the cursor_position is NOT 10th. There
    are only 9 position, so the cursor will never reach the 10th position,
    therefore the check is superfluous.

--- scripts/seek-to.lua
+++ scripts/seek-to.lua
@@ -95,9 +95,7 @@ function seek_to()
 end
 
 function backspace()
-    if cursor_position ~= 10 then
-        shift_cursor(true)
-    end
+    shift_cursor(true)
     if history[history_position][cursor_position] ~= 0 then
         copy_history_to_last()
         history[#history][cursor_position] = 0
