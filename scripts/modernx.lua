fdf3a09690600e9c8553549afa6456586c26b35a
dexeonify <bloonyapprentice@gmail.com>
2022-02-16 23:26:22 +0800

    Add link to ASS Tags

--- scripts/modernx.lua
+++ scripts/modernx.lua
@@ -63,13 +63,14 @@ local user_opts = {
     chapter_fmt = "Chapter: %s", -- chapter print format for seekbar-hover. "no" to disable
 
     titlefont = "",             -- font used for the title above OSC and
-                                -- in window controls bar
-    blur_intensity = 150,       -- adjust the strength of the blur
+                                -- in the windows controls bar
+    blur_intensity = 150,       -- adjust the strength of the OSC blur
     osc_color = "000000",       -- accent of the OSC and the title bar
     seekbarfg_color = "E39C42", -- color of the seekbar progress and handle
     seekbarbg_color = "FFFFFF", -- color of the remaining seekbar
                                 -- do note that ASS tag's color code is used,
                                 -- which is BGR, instead of the standard RGB.
+                                -- see here: https://aegi.vmoe.info/docs/3.1/ASS_Tags/
 }
 
 -- read options from config and command-line
