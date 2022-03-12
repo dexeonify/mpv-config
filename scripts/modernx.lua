feb6bd5d48ce889b7d4d4f412a2e552f1b759893
dexeonify <bloonyapprentice@gmail.com>
2022-03-12 23:41:15 +0800

    Cleanup `user_opts` comments

--- scripts/modernx.lua
+++ scripts/modernx.lua
@@ -61,7 +61,7 @@ local user_opts = {
     chapter_fmt = "Chapter: %s", -- chapter print format for seekbar-hover. "no" to disable
 
     showtitle = true,           -- show title in OSC
-    showonpause = true,         -- whether to disable the hide timeout on pause
+    showonpause = true,         -- show OSC on pause
     showonstart = true,         -- show OSC on startup or when the next file in
                                 -- playlist starts playing
     showonseek = false,         -- show OSC when seeking
@@ -71,9 +71,6 @@ local user_opts = {
     osc_color = "000000",       -- accent of the OSC and the title bar
     seekbarfg_color = "E39C42", -- color of the seekbar progress and handle
     seekbarbg_color = "FFFFFF", -- color of the remaining seekbar
-                                -- do note that ASS tag's color code is used,
-                                -- which is BGR, instead of the standard RGB.
-                                -- see here: https://aegi.vmoe.info/docs/3.1/ASS_Tags/
 }
 
 -- read options from config and command-line
