ee1d3953b4577343710d31a961ed69bd0f86b00d
dexeonify <72267349+dexeonify@users.noreply.github.com>
2023-03-28 14:39:43 +0800

    Update uosc to 4.7.0
    Upstream commits: https://github.com/tomasklaen/uosc/compare/1ed8a7a...3d55917

--- scripts/uosc.lua
+++ scripts/uosc.lua
@@ -1,5 +1,5 @@
---[[ uosc 4.6.0 - 2023-Feb-15 | https://github.com/tomasklaen/uosc ]]
-local uosc_version = '4.6.0'
+--[[ uosc 4.7.0 - 2023-Apr-15 | https://github.com/tomasklaen/uosc ]]
+local uosc_version = '4.7.0'
 
 assdraw = require('mp.assdraw')
 opt = require('mp.options')
@@ -337,9 +337,11 @@ cursor = {
 	autohide = function()
 		if not cursor.on_primary_up and not Menu:is_open() then handle_mouse_leave() end
 	end,
-	autohide_timer = mp.add_timeout(mp.get_property_native('cursor-autohide') / 1000, function()
-		cursor.autohide()
-	end),
+	autohide_timer = (function()
+		local timer = mp.add_timeout(mp.get_property_native('cursor-autohide') / 1000, function() cursor.autohide() end)
+		timer:kill()
+		return timer
+	end)(),
 	queue_autohide = function()
 		if options.autohide and not cursor.on_primary_up then
 			cursor.autohide_timer:kill()
@@ -470,17 +472,19 @@ end
 function update_margins()
 	if display.height == 0 then return end
 
-	local function is_persistent(element) return element and element.enabled and element:is_persistent() end
+	local function causes_margin(element)
+		return element and element.enabled and (element:is_persistent() or element.min_visibility > 0.5)
+	end
 	local timeline, top_bar, controls, volume = Elements.timeline, Elements.top_bar, Elements.controls, Elements.volume
 	-- margins are normalized to window size
 	local left, right, top, bottom = 0, 0, 0, 0
 
-	if is_persistent(controls) then bottom = (display.height - controls.ay) / display.height
-	elseif is_persistent(timeline) then bottom = (display.height - timeline.ay) / display.height end
+	if causes_margin(controls) then bottom = (display.height - controls.ay) / display.height
+	elseif causes_margin(timeline) then bottom = (display.height - timeline.ay) / display.height end
 
-	if is_persistent(top_bar) then top = top_bar.title_by / display.height end
+	if causes_margin(top_bar) then top = top_bar.title_by / display.height end
 
-	if is_persistent(volume) then
+	if causes_margin(volume) then
 		if options.volume == 'left' then left = volume.bx / display.width
 		elseif options.volume == 'right' then right = volume.ax / display.width end
 	end
@@ -653,6 +657,7 @@ mp.register_event('file-loaded', function()
 	Elements:flash({'top_bar'})
 end)
 mp.register_event('end-file', function(event)
+	set_state('path', nil)
 	if event.reason == 'eof' then
 		file_end_timer:kill()
 		handle_file_end()
